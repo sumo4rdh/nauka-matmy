8ea72196722c1fdb2bda7699170002f05ce2b16b
Artur Brzozowski <artur.brzozowski@zhp.net.pl>
2015-08-17 20:40:12 +0100

    dodanie menu - dodanie licznika

--- jquery.js
+++ jquery.js
@@ -3,6 +3,25 @@ $(document).ready(function() {
     $( ".rozw" ).click(function() {
         $(".rozwDzialania" ).slideToggle();
     });
+    $('.tryb').click(function() {
+    	$("#czystka").toggle();
+    });
+    $('.sprawdzsie').click(function() {
+    	$(".sprawdzian").show();
+        $(".nauczanie").hide();
+        $(".dzialania").hide();
+    });
+     $('.nauczsie').click(function() {
+    	$(".sprawdzian").hide();
+        $(".nauczanie").show();
+        $(".dzialania").hide();
+    });
+     $('.dzialajsie').click(function() {
+    	$(".sprawdzian").hide();
+        $(".nauczanie").hide();
+        $(".dzialania").show();
+    });
+    
 });
 
 
--- js.js
+++ js.js
@@ -1,89 +1,101 @@
-function fnIgnoreEnter(thisEvent)
-{
-    if(thisEvent.keyCode == 13) // enter key
-    {
+/////////////////////// SPRAWDZENIE ////////////////////
+
+function sprawdz() {
+    var p = document.getElementById('wy'), niewidz = document.getElementById('spr').value, wyna = document.getElementById('niewidzialny').value;
+    document.getElementById('odpowiedz').innerHTML = "Prawidlowa odpowiedz to:";
+    document.getElementById('obliczono').innerHTML = wyna;
+    if (wyna == niewidz) {
+        document.getElementById('opinia').innerHTML = "Twój wynik jest poprawny";
+    } else {
+        document.getElementById('opinia').innerHTML = "oj oj coś poszło nie tak :( masz zły wynik";
+
+    }
+}
+function fnIgnoreEnter(thisEvent) {
+    if (thisEvent.keyCode == 13) {
         sprawdz();
     }
 }
-var elementy = new Array('+','-','*','/');
+var elementy = ['+', '-', '*', '/'];
 var zakresliczenia = 1;
 /////////////////////// MENUE       ////////////////////
 /////////////////////// Czyszczenie ////////////////////
-function czyszczenie(){
-     document.getElementById('spr').value= "";
-     document.getElementById('odpowiedz').innerHTML = "";
-     document.getElementById('obliczono').innerHTML = ""; 
-     document.getElementById('opinia').innerHTML = ""; 
+function czyszczenie() {
+    document.getElementById('spr').value = "";
+    document.getElementById('odpowiedz').innerHTML = "";
+    document.getElementById('obliczono').innerHTML = "";
+    document.getElementById('opinia').innerHTML = "";
 }
        
 /////////////////////// Losowanie ////////////////////
 
-function losowanie(){
-        zakres();
-        czyszczenie();
-        var pierwsza = Math.round(Math.random() * (document.getElementById("zakresLiczenia").value - 1)+1);
-        var druga = Math.round(Math.random() * (document.getElementById("zakresLiczenia").value - 1)+1);
-        var obliczenie;
-        var form = document.getElementById('niewidzialny');
-        var i = Math.round(Math.random() * (elementy.length-1));
-        var z = document.getElementById('3');
-        z.innerHTML = pierwsza + "  " + elementy[i] +"  " + druga +"  =";
-        var wyn = document.getElementById('wynik');
-        switch(elementy[i]){
-        case '+':
-           obliczenie = pierwsza+druga ;
-                break;
-        case '-':
-           obliczenie = pierwsza-druga ;
-                break;
-        case '*':
-           obliczenie = pierwsza*druga ;
-                break;
-        case '/':
-           obliczenie = pierwsza/druga ;
-                break;
-        }  
-        form.value = obliczenie;
+function losowanie() {
+    zakres();
+    czyszczenie();
+    timer();
+    var pierwsza = Math.round(Math.random() * (document.getElementById("zakresLiczenia").value - 1) + 1), druga = Math.round(Math.random() * (document.getElementById("zakresLiczenia").value - 1) + 1), obliczenie, form = document.getElementById('niewidzialny'), i = Math.round(Math.random() * (elementy.length - 1)), z = document.getElementById('3'), wyn = document.getElementById('wynik');
+    z.innerHTML = pierwsza + "  " + elementy[i] + "  " + druga + "  =";
+    switch (elementy[i]) {
+    case '+':
+        obliczenie = pierwsza + druga;
+        break;
+    case '-':
+        obliczenie = pierwsza - druga;
+        break;
+    case '*':
+        obliczenie = pierwsza * druga;
+        break;
+    case '/':
+        obliczenie = pierwsza / druga;
+        break;
     }
-
-/////////////////////// SPRAWDZENIE ////////////////////
-
-function sprawdz(){
-     var p = document.getElementById('wy');
-     var niewidz = document.getElementById('spr').value;
-     var wyna = document.getElementById('niewidzialny').value;
-     document.getElementById('odpowiedz').innerHTML = "Prawidlowa odpowiedz to:";
-     document.getElementById('obliczono').innerHTML = wyna;
-
-     if (wyna == niewidz){
-       document.getElementById('opinia').innerHTML = "Twój wynik jest poprawny";
-     }else document.getElementById('opinia').innerHTML = "oj oj coś poszło nie tak :( masz zły wynik";
+    form.value = obliczenie;
 }
+
+
  /////////////////////// Ustawianie Zakresu ////////////////////  
  /////////////////////// działania         ////////////////////
-function zakres(){
+function zakres() {
     elementy = [];
-    if(document.getElementById("plus").checked){
-     elementy.push('+');
-     }
-    if(document.getElementById("minus").checked){
-     elementy.push('-');
-     }
-    if(document.getElementById("razy").checked){
-     elementy.push('*');
-     }
-    if(document.getElementById("dziel").checked){
-     elementy.push('/');
-     }
+    if (document.getElementById("plus").checked) {
+        elementy.push('+');
+    }
+    if (document.getElementById("minus").checked) {
+        elementy.push('-');
+    }
+    if (document.getElementById("razy").checked) {
+        elementy.push('*');
+    }
+    if (document.getElementById("dziel").checked) {
+        elementy.push('/');
+    }
 }
  /////////////////////// Wartości do obliczenia ////////////////////
-function increment(){
+function increment() {
     zakresliczenia = document.getElementById("zakresLiczenia").value; 
     zakresliczenia = parseInt(zakresliczenia) + 1;
     document.getElementById("zakresLiczenia").value = zakresliczenia;
 }
-function decrement(){
+function decrement() {
     zakresliczenia = document.getElementById("zakresLiczenia").value; 
     zakresliczenia = parseInt(zakresliczenia) - 1;
     document.getElementById("zakresLiczenia").value = zakresliczenia;    
+}
+
+/////////////////////// Timer ////////////////////
+function timer(){
+    
+    var myVar = setInterval(function(){myTimer()},1000);
+    var d = 10;
+    function myTimer() {
+    
+        d -=1;
+            if (d <= 0){
+                document.getElementById("licznik").innerHTML = "";
+                sprawdz();
+                clearInterval(myTimer());
+                
+            }
+        document.getElementById("licznik").innerHTML = d;
+    }
 }
